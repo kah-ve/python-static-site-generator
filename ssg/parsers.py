d3f3974ab12665f7006bb1b42914c7f8c6a69c7b
kah-ve <cam.abdurrahman@gmail.com>
2020-11-15 15:39:04 -0500

    Module2 2

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -13,13 +13,13 @@ class Parser:
         raise NotImplementedError
     
     def read(self, path):
-        with open(path) as file:
+        with open(path, "r") as file:
             return file.read()
     
     def write(self, path, dest, content, ext=".html"):
         full_path: str = dest / path.with_suffix(ext).name
     
-        with open(full_path) as file:
+        with open(full_path, "w") as file:
             file.write(content)
 
     def copy(self, path: Path, source, dest):
